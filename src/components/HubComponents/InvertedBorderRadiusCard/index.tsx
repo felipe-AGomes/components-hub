c62ebe31deb4258041108bce8bbee6dddbd93ab6
felipe-AGomes <falmeidagomes13@gmail.com>
2023-06-10 11:38:38 -0300

    feat: separate miniature and normal code

--- src/components/HubComponents/InvertedBorderRadiusCard/index.tsx
+++ src/components/HubComponents/InvertedBorderRadiusCard/index.tsx
@@ -1,26 +1,78 @@
+/* eslint-disable @next/next/no-img-element */
+import ContainMiniature from '@/components/ContainMiniature';
 import S from './InvertedBorderRadiusCard.module.css';
+import useAppContext from '@/hooks/useAppContext';
 
-export default function InvertedBorderRadiusCard() {
+type Props = {
+	isMiniature: boolean;
+};
+
+export default function InvertedBorderRadiusCard({ isMiniature }: Props) {
+	const { miniatures } = useAppContext();
+
+	const miniature = miniatures.find(
+		(miniature) => miniature.id === 'invertedBorderRadiusCard',
+	);
+
+	if (!miniature) {
+		throw new Error('Miniatura não existe');
+	}
+
 	return (
-		<section
-			className='component__page'
-			id={S.contain}
-		>
-			<div className={S.box}>
-				<div className={S.card}>
-					<div className={S.imgBx}></div>
-					<div className={S.content}>
-						<span className={S.price}>
-							<a href='#'>$1.000,00</a>
-						</span>
-						<ul>
-							<li>Somewhere</li>
-							<li>1200sqm</li>
-							<li>House</li>
-						</ul>
+		<>
+			{isMiniature ? (
+				<ContainMiniature
+					style={{ background: '#bfc0c0' }}
+					miniature={miniature}
+				>
+					<div className={S.boxMiniature}>
+						<div className={S.cardMiniature}>
+							<div className={S.imgBxMiniature}>
+								<img
+									src='/InvertedBorderRadiusCard/house.jpg'
+									alt='house image'
+								/>
+							</div>
+							<div className={S.contentMiniature}>
+								<span className={S.priceMiniature}>
+									<a href='#'>$1.000,00</a>
+								</span>
+								<ul>
+									<li>Somewhere</li>
+									<li>1200sqm</li>
+									<li>House</li>
+								</ul>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-		</section>
+				</ContainMiniature>
+			) : (
+				<section
+					className='component__page'
+					id={S.contain}
+				>
+					<div className={S.box}>
+						<div className={S.card}>
+							<div className={S.imgBx}>
+								<img
+									src='/InvertedBorderRadiusCard/house.jpg'
+									alt='house image'
+								/>
+							</div>
+							<div className={S.content}>
+								<span className={S.price}>
+									<a href='#'>$1.000,00</a>
+								</span>
+								<ul>
+									<li>Somewhere</li>
+									<li>1200sqm</li>
+									<li>House</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+			)}
+		</>
 	);
 }
